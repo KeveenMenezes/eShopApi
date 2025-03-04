3878190a81654ffd4bf8bb83ef43ee556a7db717
Keveen Menezes <endykeven@gmail.com>
2025-03-04 04:34:55 -0300

    Update project paths to use consistent casing in BuildingBlocks and Catalog.API

--- src/BuildingBlocks/obj/Debug/net9.0/BuildingBlocks.AssemblyInfo.cs
+++ src/BuildingBlocks/obj/Debug/net9.0/BuildingBlocks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a182b74b162a5e5f790724cf6ffc17ff616aabf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ba1baaff895febd4a96d503bab703643a19c92f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
